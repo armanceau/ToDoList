7bba8f0966e55c168e5c74611aa9cecb264485bd
armanceau <100920811+armanceau@users.noreply.github.com>
2023-04-15 17:28:39 +0200

    CSS
    Add a little css on the tilte, input and list

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -33,28 +33,49 @@ function deleteItem(id){
 }
 
   return (
+
     <div className="App">
+
       {/* 1. Header */}
-      <h1>Todo List App</h1>
+      <section className='header'>
+        <div className='divHeader'>
+          <h1>Todo List App</h1>
+        </div>
+      </section>
 
       {/* 2. Input (input and button) */}
-      <input
-        type="text"
-        placeholder='Ajouter un item ...'
-        value={newItem}
-        onChange={e => setNewItem(e.target.value)}
-      />
+      <section className='main'>
 
-      <button onClick={() => addItem()}>Ajouter</button>
+        <div className='divMain'>
+          <input 
+            className='add-input'
+            type="text"
+            placeholder='Ajouter un item ...'
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+          />
 
-      {/* 3. List of items (unordered list with list items) */}
-      <ul>
-        {items.map(item => {
-          return(
-            <li key={item.id}>{item.value} <button className='delete-button' onClick={() => deleteItem(item.id)}>🗙</button></li>
-          )
-        })}
-      </ul>
+          <button className='add-button' onClick={() => addItem()}>➕</button>
+        </div>
+        
+        <div>
+          {/* 3. List of items (unordered list with list items) */}
+          <ul>
+            {items.map(item => {
+              return(
+                <li key={item.id}><button className='delete-button' onClick={() => deleteItem(item.id)}>🗙</button>{item.value}</li>
+              )
+            })}
+          </ul>
+        </div>
+        
+      
+      </section>
+
+      
+
+      
+
     </div>
   );
 }
